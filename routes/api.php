d4420345f4579f303bbe7a36ffe76821526d1b3a
mathfajardo <mathfajardo.contato@hotmail.com>
2025-11-19 20:13:08 -0300

    ajustando rotas

--- routes/api.php
+++ routes/api.php
@@ -12,27 +12,27 @@ use Illuminate\Support\Facades\Route;
 
 // rotas dos produtos
 Route::middleware('auth:sanctum')->group(function () {
+    // rota dos produtos
     Route::get('/produtos', [ProdutosController::class, 'index'])->middleware('ability:get-produtos');
+    Route::post('/produtos', [ProdutosController::class, 'store']);
+    Route::get('/produtos/{produto}', [ProdutosController::class, 'show']);
+    Route::delete('/produtos/{produto}', [ProdutosController::class, 'destroy']);
+    Route::put('/produtos/{produto}', [ProdutosController::class, "update"]);
+
+    // rotas dos clientes
+    Route::get('/clientes', [ClientesController::class, 'index']);
+    Route::post('/clientes', [ClientesController::class, 'store']);
+    Route::get('/clientes/{cliente}', [ClientesController::class, 'show']);
+    Route::delete('/clientes/{cliente}', [ClientesController::class, 'destroy']);
+
+    // rotas das vendas
+    Route::get('/vendas', [VendasController::class, 'index']);
+    Route::post('/vendas', [VendasController::class, 'store']);
+    Route::get('/vendas/{venda}', [VendasController::class, 'show']);
+    Route::delete('/vendas/{venda}', [VendasController::class, 'destroy']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
-
-
-Route::post('/produtos', [ProdutosController::class, 'store']);
-Route::get('/produtos/{produto}', [ProdutosController::class, 'show']);
-Route::delete('/produtos/{produto}', [ProdutosController::class, 'destroy']);
-Route::put('/produtos/{produto}', [ProdutosController::class, "update"]);
-
-// rotas dos clientes
-Route::get('/clientes', [ClientesController::class, 'index']);
-Route::post('/clientes', [ClientesController::class, 'store']);
-Route::get('/clientes/{cliente}', [ClientesController::class, 'show']);
-Route::delete('/clientes/{cliente}', [ClientesController::class, 'destroy']);
-
-// rotas das vendas
-Route::get('/vendas', [VendasController::class, 'index']);
-Route::post('/vendas', [VendasController::class, 'store']);
-Route::get('/vendas/{venda}', [VendasController::class, 'show']);
-Route::delete('/vendas/{venda}', [VendasController::class, 'destroy']);
 
 // rota para autenticação
 Route::post('/login', [AuthController::class, 'login']);
